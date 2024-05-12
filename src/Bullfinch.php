f58ed7a919b2c39a929c437b7bebbdcfd95f02fb
Loren Supernaw <loren.supernaw@gmail.com>
2024-05-12 10:38:22 -0500

    initial commit

---
+++ src/Bullfinch.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NestboxPHP\Bullfinch;
+
+use NestboxPHP\Nestbox\Nestbox;
+
+class Bullfinch extends Nestbox
+{
+    final protected const string PACKAGE_NAME = 'bullfinch';
+}
